b39a0dabbfbdfbf4f90ae0f06286f7e700418347
Abdul Kadir <abdulkadir112k@gmail.com>
2024-07-09 13:24:20 +0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+
+// service part slider js..........
+$('.banner_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed:1900,
+    prevArrow:false,
+    nextArrow:false,
+  });
+
+// =================================================
+
+$('.bnnerSlider').slick({
+  dots: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+  autoplay:true,
+  autoplaySpeed:1500,
+  arrows: false,
+});
+
+$('.menuSlider').slick({
+  dots: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+  autoplay:true,
+  autoplaySpeed:800,
+  arrows: false,
+});
+
+
+$('.photoSlider').slick({
+dots: false,
+arrows: true,
+infinite: true,
+slidesToShow: 3,
+slidesToScroll: 3,
+centerMode: true,
+centerPadding: '60px',
+autoplay:false,
+prevArrow: false,
+nextArrow: false,
+})
+
+$('.bnrSlider').slick({
+dots:false,
+arrows:false,
+autoplay:true,
+});
